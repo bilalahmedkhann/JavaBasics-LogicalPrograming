a049a771456da41d19ed6e56d3d4cf58e253573a
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-21 23:50:27 +0530

    ADDED: Program for checking Perfect number

--- LogicalPrograms.java
+++ LogicalPrograms.java
@@ -4,6 +4,7 @@ public class LogicalPrograms {
     public static void main(String[] args) {
         LogicalPrograms logic = new LogicalPrograms();
         logic.fibonacci();
+        logic.perfectNumber();
     }
     public void fibonacci() {
         // Initialization Fibonacci Series {0,1,1,2,3,5,8...n}
@@ -22,6 +23,26 @@ public class LogicalPrograms {
             a = b;
             b = c;
         }
+    }
+    public void perfectNumber() {
+        Scanner sc = new Scanner(System.in);
+        // get input from user
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            while (num % i == 0) {
+                System.out.println(i);
+                sum = sum + i;
+                break;
+            }
+        }
+        // check sum of its divisor equal to the input number
+        if (num == sum) {
+            System.out.println(num + " is a Perfect Number");
+        } else
+            System.out.println(num + " is Not a Perfect Number");
     }
 }
 
